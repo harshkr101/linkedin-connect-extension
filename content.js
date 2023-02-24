91477057c871eb9dd4e11c9311cc2a082468aae0
harshkr101 <harsh.mca19.du@gmail.com>
2023-02-24 18:32:57 +0530

    fixes bugs | minor changes

--- content.js
+++ content.js
@@ -13,7 +13,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 // Function to send connection request
-function connect() {
+async function connect() {
   console.log('Sending connection request');
   //get all buttons on page
   const buttons = document.getElementsByClassName('artdeco-button artdeco-button--2 artdeco-button--secondary ember-view');
@@ -25,18 +25,18 @@ function connect() {
     const timeoutId = setTimeout(() => {
       if (connectButton) {
         connectButton.click();
-        const sendButton = document.getElementsByClassName('artdeco-button artdeco-button--2 artdeco-button--primary ember-view ml1')
-        console.log('Send button',sendButton);
-        if(sendButton){
-          sendButton.click();
-          console.log("Send request for ",i+1);
+        const sendButton = Array.from(document.getElementsByClassName('artdeco-button artdeco-button--2 artdeco-button--primary ember-view ml1'));
+        if(sendButton.length > 0){
+          sendButton[0].click();
           console.log('Connection request sent');
           chrome.runtime.sendMessage({action: 'success'});
+        }else{
+          console.log('Unable to send connect request')
         }
       } else {
-        console.log('No send button found');
+        console.log('No connect button found');
       }
-    }, 5000 * i+1); // Use a different delay for each button
+    }, randomNumber(3000,6000));
 
     // Add the timeout ID to the array
     timeouts.push(timeoutId);
@@ -52,4 +52,10 @@ function stop() {
   timeouts.forEach(timeoutId => clearTimeout(timeoutId));
   }
   return;
+}
+
+
+// Function to generate random number between range
+function randomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
